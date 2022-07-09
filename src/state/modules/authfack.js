d5509355535f03edabf728e3285e422ca1f79348
erza <58721335+erzaffasya@users.noreply.github.com>
2022-07-09 18:56:15 +0800

    setting router

--- src/state/modules/authfack.js
+++ src/state/modules/authfack.js
@@ -2,14 +2,20 @@ import { userService } from '../../helpers/authservice/user.service';
 import router from '../../router/index'
 
 const user = JSON.parse(localStorage.getItem('user'));
-export const state = user
-    ? { status: { loggeduser: true }, user }
-    : { status: {}, user: null };
-
+export const state = {
+    currentUser: user,
+}
+export const getters = {
+    // Whether the user is currently logged in.
+    loggedIn(state) {
+        return !!state.currentUser
+    }
+}
 export const actions = {
     // Logs in the user.
     // eslint-disable-next-line no-unused-vars
-    login({ dispatch, commit }, { email, password }) {
+    login({ dispatch, commit, getters }, { email, password }) {
+        if (getters.loggedIn) return dispatch('validate')
         commit('loginRequest', { email });
 
         userService.login(email, password)
@@ -44,25 +50,31 @@ export const actions = {
                     dispatch('notification/error', error, { root: true });
                 }
             );
-    }
+    },
+    validate({ state }) {
+        if (!state.currentUser) return Promise.resolve(null)
+        // const user = getFirebaseBackend().getAuthenticatedUser();
+        // commit('SET_CURRENT_USER', user)
+        return state.currentUser;
+    },
 };
 
 export const mutations = {
     loginRequest(state, user) {
         state.status = { loggingIn: true };
-        state.user = user;
+        state.currentUser = user;
     },
     loginSuccess(state, user) {
         state.status = { loggeduser: true };
-        state.user = user;
+        state.currentUser = user;
     },
     loginFailure(state) {
         state.status = {};
-        state.user = null;
+        state.currentUser = null;
     },
     logout(state) {
         state.status = {};
-        state.user = null;
+        state.currentUser = null;
     },
     registerRequest(state) {
         state.status = { registering: true };
@@ -72,6 +84,10 @@ export const mutations = {
     },
     registerFailure(state) {
         state.status = {};
-    }
+    },
+    // SET_CURRENT_USER(state, newValue) {
+    //     state.currentUser = newValue
+    //     saveState('auth.currentUser', newValue)
+    // }
 };
 
